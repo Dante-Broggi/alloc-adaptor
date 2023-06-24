11fbc83f0ab70383f1c2d1b23b174b9e84f00e91
Dante-Broggi <34220985+Dante-Broggi@users.noreply.github.com>
2023-06-24 01:10:32 -0400

    test all defaults for Null, and fix warnings

--- src/null.rs
+++ src/null.rs
@@ -4,12 +4,59 @@ use crate::QueryAlloc;
 pub struct Null;
 
 unsafe impl Allocator for Null {
-    fn allocate(&self, layout: std::alloc::Layout) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+    fn allocate(&self, _layout: std::alloc::Layout) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
         Err(std::alloc::AllocError)
     }
 
-    unsafe fn deallocate(&self, ptr: std::ptr::NonNull<u8>, layout: std::alloc::Layout) {
+    unsafe fn deallocate(&self, _ptr: std::ptr::NonNull<u8>, _layout: std::alloc::Layout) {
         panic!()
+    }
+
+    fn allocate_zeroed(&self, _layout: std::alloc::Layout) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+        Err(std::alloc::AllocError)
+    }
+
+    unsafe fn grow(
+        &self,
+        _ptr: std::ptr::NonNull<u8>,
+        _old_layout: std::alloc::Layout,
+        _new_layout: std::alloc::Layout,
+    ) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+        // we cannot get here as no pointers are ever "currently allocated"
+        panic!()
+        // the effective default impl
+        // Err(std::alloc::AllocError)
+    }
+
+    unsafe fn grow_zeroed(
+        &self,
+        _ptr: std::ptr::NonNull<u8>,
+        _old_layout: std::alloc::Layout,
+        _new_layout: std::alloc::Layout,
+    ) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+        // we cannot get here as no pointers are ever "currently allocated"
+        panic!()
+        // the effective default impl
+        // Err(std::alloc::AllocError)
+    }
+
+    unsafe fn shrink(
+        &self,
+        _ptr: std::ptr::NonNull<u8>,
+        _old_layout: std::alloc::Layout,
+        _new_layout: std::alloc::Layout,
+    ) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+        // we cannot get here as no pointers are ever "currently allocated"
+        panic!()
+        // the effective default impl
+        // Err(std::alloc::AllocError)
+    }
+
+    fn by_ref(&self) -> &Self
+    where
+        Self: Sized,
+    {
+        self
     }
 }
 
