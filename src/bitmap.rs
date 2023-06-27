3f0f454b04816a454e0cbd7002b0426bc0820f4c
Dante-Broggi <34220985+Dante-Broggi@users.noreply.github.com>
2023-06-27 00:53:11 -0400

    Bitmap Allocation?

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -55,8 +55,8 @@ impl<A, const BLOCK_SIZE: usize> BitmappedBlock<A, BLOCK_SIZE> {
     least one zero bit. Assumes all control words to the left of $(D
     _control[_startIdx]) are already occupied.
     */
-    fn adjustStartIdx(&self) {
-        let mut control = self.control.get().unwrap();
+    fn adjust_start_idx(&self) {
+        let control = self.control.get().unwrap();
         while self.start_idx.get() < control.len() 
         && control[self.start_idx.get()].get() == u64::MAX
         {
@@ -68,10 +68,10 @@ impl<A, const BLOCK_SIZE: usize> BitmappedBlock<A, BLOCK_SIZE> {
     Returns the blocks corresponding to the control bits starting at word index
     wordIdx and bit index msbIdx (MSB=0) for a total of howManyBlocks.
     */
-    fn blocksFor(&self, wordIdx: usize, msbIdx: u8, howManyBlocks: usize) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
-        assert!(msbIdx <= 63);
-        let start = (wordIdx * 64 + Into::<usize>::into(msbIdx)) * BLOCK_SIZE;
-        let end = start + BLOCK_SIZE * howManyBlocks;
+    fn blocks_for(&self, word_idx: usize, msb_idx: u8, how_many_blocks: usize) -> Result<NonNull<[u8]>, std::alloc::AllocError> {
+        assert!(msb_idx <= 63);
+        let start = (word_idx * 64 + Into::<usize>::into(msb_idx)) * BLOCK_SIZE;
+        let end = start + BLOCK_SIZE * how_many_blocks;
         let payload = self.payload.get().unwrap();
         if end <= payload.len() { 
             unsafe {
@@ -90,43 +90,43 @@ impl<A, const BLOCK_SIZE: usize> BitmappedBlock<A, BLOCK_SIZE> {
     it succeeds, fills "result" with the result and returns tuple(size_t.max,
     0). Otherwise, returns a tuple with the next position to search.
     */
-    fn allocateAt(&self, wordIdx: usize, msbIdx: u8, blocks: usize, result: &mut Result<NonNull<[u8]>, std::alloc::AllocError>) -> (usize, u8) {
+    fn allocate_at(&self, word_idx: usize, msb_idx: u8, blocks: usize, result: &mut Result<NonNull<[u8]>, std::alloc::AllocError>) -> (usize, u8) {
         // SAFETY: we have thread local access to `self.control`
         let control = self.control.get().unwrap();
         assert!(blocks > 0);
-        assert!(wordIdx < control.len());
-        assert!(msbIdx <= 63);
-        if Into::<usize>::into(msbIdx) + blocks <= 64 {
+        assert!(word_idx < control.len());
+        assert!(msb_idx <= 63);
+        if Into::<usize>::into(msb_idx) + blocks <= 64 {
             let blocks: u8 = blocks.try_into().unwrap();
             // Allocation should fit this control word
-            if set_bits_if_zero(&control[wordIdx], 64 - msbIdx - blocks, 63 - msbIdx) {
+            if set_bits_if_zero(&control[word_idx], 64 - msb_idx - blocks, 63 - msb_idx) {
                 // Success
-                *result = self.blocksFor(wordIdx, msbIdx, blocks.into());
+                *result = self.blocks_for(word_idx, msb_idx, blocks.into());
                 return (usize::MAX, 0);
             }
             // Can't allocate, make a suggestion
-            return if msbIdx + blocks == 64
-                { (wordIdx + 1, 0) } else
-                { (wordIdx, (msbIdx + blocks)) };
+            return if msb_idx + blocks == 64
+                { (word_idx + 1, 0) } else
+                { (word_idx, (msb_idx + blocks)) };
         }
         // Allocation spans two control words or more
-        let mask = u64::MAX >> msbIdx;
-        if (control[wordIdx].get() & mask) != 0 {
+        let mask = u64::MAX >> msb_idx;
+        if (control[word_idx].get() & mask) != 0 {
             // We can't allocate the rest of this control word,
             // return a suggestion.
-            return (wordIdx + 1, 0);
+            return (word_idx + 1, 0);
         }
         // We can allocate the rest of this control word, but we first need to
         // make sure we can allocate the tail.
-        if wordIdx + 1 == control.len() {
+        if word_idx + 1 == control.len() {
             // No more memory
             return (control.len(), 0);
         }
-        let hint = self.allocateAt(wordIdx + 1, 0, blocks - 64 + Into::<usize>::into(msbIdx), result);
+        let hint = self.allocate_at(word_idx + 1, 0, blocks - 64 + Into::<usize>::into(msb_idx), result);
         if hint.0 == usize::MAX {
             // We did it!
-            control[wordIdx].set(control[wordIdx].get() | mask);
-            *result = self.blocksFor(wordIdx, msbIdx, blocks);
+            control[word_idx].set(control[word_idx].get() | mask);
+            *result = self.blocks_for(word_idx, msb_idx, blocks);
             return (usize::MAX, 0);
         }
         // Failed, return a suggestion that skips this whole run.
@@ -135,27 +135,27 @@ impl<A, const BLOCK_SIZE: usize> BitmappedBlock<A, BLOCK_SIZE> {
 
     /** Allocates as many blocks as possible at the end of the blocks indicated
     by wordIdx. Returns the number of blocks allocated. */
-    fn allocateAtTail(&self, wordIdx: usize) -> u32 {
+    fn allocate_at_tail(&self, word_idx: usize) -> u32 {
         let control = self.control.get().unwrap();
-        assert!(wordIdx < control.len());
-        let available = (control[wordIdx].get()).trailing_zeros();
-        control[wordIdx].set(control[wordIdx].get() | u64::MAX >> available);
+        assert!(word_idx < control.len());
+        let available = (control[word_idx].get()).trailing_zeros();
+        control[word_idx].set(control[word_idx].get() | u64::MAX >> available);
         return available;
     }
 
-    fn smallAlloc(&self, blocks: u8) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+    fn small_alloc(&self, blocks: u8) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
         assert!(blocks >= 2 && blocks <= 64);
         let control = self.control.get().unwrap();
         for i in self.start_idx.get() .. control.len() {
             // Test within the current 64-bit word
             let v = control[i].get();
             if v == u64::MAX { continue };
-            let j = findContigOnes(!v, blocks);
+            let j = find_contig_ones(!v, blocks);
             if j < 64 {
                 let j = j.try_into().unwrap();
                 // yay, found stuff
-                setBits(&control[i], 64 - j - blocks, 63 - j);
-                return self.blocksFor(i, j, blocks.into());
+                set_bits(&control[i], 64 - j - blocks, 63 - j);
+                return self.blocks_for(i, j, blocks.into());
             }
             // Next, try allocations that cross a word
             let available: u8 = v.trailing_zeros().try_into().unwrap();
@@ -164,16 +164,16 @@ impl<A, const BLOCK_SIZE: usize> BitmappedBlock<A, BLOCK_SIZE> {
             assert!(available < blocks.into()); // otherwise we should have found it
             let needed = blocks - available;
             assert!(needed > 0 && needed < 64);
-            if self.allocateAtFront(i + 1, needed) {
+            if self.allocate_at_front(i + 1, needed) {
                 // yay, found a block crossing two words
                 control[i].set(control[i].get() | (1_u64 << available) - 1);
-                return self.blocksFor(i, 64 - available, blocks.into());
+                return self.blocks_for(i, 64 - available, blocks.into());
             }
         }
         return Err(std::alloc::AllocError);
     }
 
-    fn hugeAlloc(&self, blocks: usize) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+    fn huge_alloc(&self, blocks: usize) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
         assert!(blocks > 64);
         let mut result = Err(std::alloc::AllocError);
         let mut pos = (self.start_idx.get(), 0);
@@ -183,7 +183,7 @@ impl<A, const BLOCK_SIZE: usize> BitmappedBlock<A, BLOCK_SIZE> {
                 // No more memory
                 return Err(std::alloc::AllocError);
             }
-            pos = self.allocateAt(pos.0, pos.1, blocks, &mut result);
+            pos = self.allocate_at(pos.0, pos.1, blocks, &mut result);
             if pos.0 == usize::MAX {
                 // Found and allocated
                 return result;
@@ -198,15 +198,13 @@ impl<A, const BLOCK_SIZE: usize> BitmappedBlock<A, BLOCK_SIZE> {
 
     /** Allocates given blocks at the beginning blocks indicated by wordIdx.
     Returns true if allocation was possible, false otherwise. */
-    fn allocateAtFront(&self, wordIdx: usize, blocks: u8) -> bool {
+    fn allocate_at_front(&self, word_idx: usize, blocks: u8) -> bool {
         let control = self.control.get().unwrap();
-        assert!(wordIdx < control.len() && blocks >= 1 && blocks <= 64);
+        assert!(word_idx < control.len() && blocks >= 1 && blocks <= 64);
         let mask = (1 << (64 - blocks)) - 1;
-        unsafe {
-            if control[wordIdx].get() > mask { return false };
-            // yay, works
-            control[wordIdx].set(control[wordIdx].get() | !mask);
-        }
+        if control[word_idx].get() > mask { return false };
+        // yay, works
+        control[word_idx].set(control[word_idx].get() | !mask);
         return true;
     } 
 }
@@ -217,7 +215,41 @@ unsafe impl<A: Allocator, const BLOCK_SIZE: usize> Allocator for BitmappedBlock<
             // FIXME: handle aligned alloc requests
             return Err(std::alloc::AllocError);
         }
-        todo!()
+        if self.control.get().is_none() {
+            // Lazy initialize
+        }
+        assert!((self.blocks != 0) && self.control.get().is_some() && self.payload.get().is_some());
+        let control = self.control.get().unwrap();
+        let blocks = Self::bytes2blocks(layout.size());
+        
+        match blocks {
+            1 => {
+                // inline code here for speed
+                // find the next available block
+                for i in self.start_idx.get() .. control.len() {
+                    let w = control[i].get();
+                    if w == u64::MAX { continue };
+                    let j: u8 = w.leading_ones().try_into().unwrap();
+                    assert!(j < 64);
+                    assert!((control[i].get() & ((1_u64 << 63) >> j)) == 0);
+                    control[i].set(control[i].get() | (1_u64 << 63) >> j);
+                    if i == self.start_idx.get() {
+                        self.adjust_start_idx();
+                    }
+                    return self.blocks_for(i, j, 1);
+                }
+                Err(std::alloc::AllocError)
+            },
+            0 => {
+                Err(std::alloc::AllocError)
+            },
+            2..=63 => {
+                self.small_alloc(blocks.try_into().unwrap())
+            },
+            _ => {
+                self.huge_alloc(blocks)
+            }
+        }
     }
 
     unsafe fn deallocate(&self, ptr: std::ptr::NonNull<u8>, layout: std::alloc::Layout) {
@@ -226,38 +258,38 @@ unsafe impl<A: Allocator, const BLOCK_SIZE: usize> Allocator for BitmappedBlock<
         // Locate position
         let pos = ptr.as_ptr().sub_ptr(self.payload.get().unwrap().as_mut_ptr());
         assert_eq!(pos % BLOCK_SIZE, 0);
-        let blockIdx = pos / BLOCK_SIZE;
-        let mut wordIdx = blockIdx / 64;
-        let mut msbIdx: u8 = (blockIdx % 64).try_into().unwrap();
-        if self.start_idx.get() > wordIdx { self.start_idx.set(wordIdx) };
+        let block_idx = pos / BLOCK_SIZE;
+        let mut word_idx = block_idx / 64;
+        let mut msb_idx: u8 = (block_idx % 64).try_into().unwrap();
+        if self.start_idx.get() > word_idx { self.start_idx.set(word_idx) };
 
         let control = self.control.get().unwrap();
         
         // Three stages: heading bits, full words, leftover bits
-        if msbIdx != 0 {
-            if blocks + Into::<usize>::into(msbIdx) <= 64 {
+        if msb_idx != 0 {
+            if blocks + Into::<usize>::into(msb_idx) <= 64 {
                 let blocks: u8 = blocks.try_into().unwrap();
-                reset_bits(&control[wordIdx], 64 - msbIdx - blocks, 63 - msbIdx);
+                reset_bits(&control[word_idx], 64 - msb_idx - blocks, 63 - msb_idx);
                 return;
             } else {
-                control[wordIdx].set(control[wordIdx].get() & u64::MAX << 64 - msbIdx);
-                blocks -= 64_usize - Into::<usize>::into(msbIdx);
-                wordIdx += 1;
-                msbIdx = 0;
+                control[word_idx].set(control[word_idx].get() & u64::MAX << 64 - msb_idx);
+                blocks -= 64_usize - Into::<usize>::into(msb_idx);
+                word_idx += 1;
+                msb_idx = 0;
             }
         }
         
         // Stage 2: reset one word at a time
         while blocks >= 64 {
-            control[wordIdx].set(0);
-            wordIdx += 1;
+            control[word_idx].set(0);
+            word_idx += 1;
             blocks -= 64;
         }
         
         // Stage 3: deal with leftover bits, if any
-        assert!(wordIdx <= control.len());
+        assert!(word_idx <= control.len());
         if blocks != 0 {
-            control[wordIdx].set(control[wordIdx].get() & u64::MAX >> blocks);
+            control[word_idx].set(control[word_idx].get() & u64::MAX >> blocks);
         }
     }
 }
@@ -295,7 +327,7 @@ Finds a run of contiguous ones in $(D x) of length at least $(D n).
 ```
 
 */
-pub fn findContigOnes(mut x: u64, mut n: u8) -> u32 {
+pub fn find_contig_ones(mut x: u64, mut n: u8) -> u32 {
     while n > 1 {
         let s = n >> 1;
         x &= x << s;
@@ -319,16 +351,16 @@ const _: () = ();
 Unconditionally sets the bits from lsb through msb in w to zero.
 
 ```
-    use alloc_adaptor::bitmap::setBits;
+    use alloc_adaptor::bitmap::set_bits;
     let mut w: u64;
-    w = 0; setBits(&mut w, 0, 63); assert_eq!(w, u64::MAX);
-    w = 0; setBits(&mut w, 1, 63); assert_eq!(w, u64::MAX - 1);
-    w = 6; setBits(&mut w, 0, 1); assert_eq!(w, 7);
-    w = 6; setBits(&mut w, 3, 3); assert_eq!(w, 14);
+    w = 0; set_bits(&mut w, 0, 63); assert_eq!(w, u64::MAX);
+    w = 0; set_bits(&mut w, 1, 63); assert_eq!(w, u64::MAX - 1);
+    w = 6; set_bits(&mut w, 0, 1); assert_eq!(w, 7);
+    w = 6; set_bits(&mut w, 3, 3); assert_eq!(w, 14);
 ```
 
 */
-pub fn setBits(w: &Cell<u64>, lsb: u8, msb: u8) {
+pub fn set_bits(w: &Cell<u64>, lsb: u8, msb: u8) {
     assert!(lsb <= msb && msb < 64);
     let mask = (u64::MAX << lsb) & (u64::MAX >> (63 - msb));
     w.set(w.get() | mask);
